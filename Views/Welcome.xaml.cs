bcda78b50da177b0e7e935d68caa233e5b378af5
Thanh Long Cao <ctlong26051993@gmail.com>
2022-06-06 13:29:20 +0900

    Add connection functionality

--- Views/Welcome.xaml.cs
+++ Views/Welcome.xaml.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Dynamic;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace PC_Client.Views
@@ -16,27 +19,39 @@ namespace PC_Client.Views
     /// </summary>
     public partial class Welcome : Window
     {
-        Process process = new Process();
+        static string ip;
+        static int port = 8886;
+        private Process process = new Process();
         private ServerConnection connection;
 
         private Dictionary<string, Phone> history = new Dictionary<string, Phone>();
         private Dictionary<string, Phone> activePhones = new Dictionary<string, Phone>();
         private string currentMac;
 
-        RemoteVideoTrack remoteVideoTrack;
-        RemoteAudioTrack remoteAudioTrack;
-        private List<PeerConnection> peerConnections = new List<PeerConnection>();
+        private DeviceAudioTrackSource microphoneSource;
+        private DeviceVideoTrackSource webcamSource;
+        private LocalAudioTrack localAudioTrack;
+        private LocalVideoTrack localVideoTrack;
+
+        private RemoteVideoTrack remoteVideoTrack;
+        private RemoteAudioTrack remoteAudioTrack;
+
+        private Transceiver videoTransceiver;
+        private Transceiver audioTransceiver;
+
+        private PeerConnection peerConnection;
 
         public Welcome()
         {
             InitializeComponent();
-            //StartSignalingServer();
-            //SetupServerConnection("localhost", "8886"); // temp
-            //ConnectToServer();
-            //LoadHistory();
+            StartServer();
+            SetupConnection();
+            ConnectToServer();
+            CaptureLocalMedia();
+            SetUpPeerConnection();
         }
 
-        private void StartSignalingServer()
+        private void StartServer()
         {
             process.StartInfo.FileName = "cmd.exe";
             string server_path = Path.Combine(Environment.CurrentDirectory, "signaling_server.js");
@@ -44,166 +59,195 @@ namespace PC_Client.Views
             process.Start();
         }
 
-        private void SetupServerConnection(string ip, string port)
+        private void SetupConnection()
         {
-            connection = new ServerConnection(ip, port);
+            ip = GetLocalIpAddress();
+            Console.WriteLine(ip);
+            connection = new ServerConnection(ip, port.ToString());
             connection.WebSocket.Connect();
-            connection.WebSocket.OnOpen += WebSocket_OnOpen;
             connection.WebSocket.OnMessage += WebSocket_OnMessage;
-            connection.WebSocket.OnError += WebSocket_OnError;
-            connection.WebSocket.OnClose += WebSocket_OnClose;
+        }
+
+        private string GetLocalIpAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return null;
         }
 
         private void ConnectToServer()
         {
-
             var pcName = Environment.MachineName;
             var os = Environment.OSVersion.ToString();
 
-            string message = PCMessage.ConnnectToServer("ip1", pcName, os);
+            string message = PCMessage.ConnnectToServer(ip, pcName, os);
+
             bool result = connection.SendMessage(message);
 
             if (!result)
             {
-                MessageBox.Show("Server died");
+                Console.WriteLine("Sever died");
             }
         }
-
-        private void SaveHistory()
+        private async Task CaptureLocalMedia()
         {
-            string path = Path.Combine(Environment.CurrentDirectory, "history.json");
-
-            using (StreamWriter file = File.CreateText(path))
+            // Video
+            webcamSource = await DeviceVideoTrackSource.CreateAsync();
+            var videoTrackConfig = new LocalVideoTrackInitConfig
             {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, history);
+                trackName = "webcam_track"
             };
+            localVideoTrack = LocalVideoTrack.CreateFromSource(webcamSource, videoTrackConfig);
+
+            // Audio
+            microphoneSource = await DeviceAudioTrackSource.CreateAsync();
+            var audioTrackConfig = new LocalAudioTrackInitConfig
+            {
+                trackName = "microphone_track",
+
+            };
+            localAudioTrack = LocalAudioTrack.CreateFromSource(microphoneSource, audioTrackConfig);
         }
 
-        private void LoadHistory()
+        private async Task SetUpPeerConnection()
         {
-            string path = Path.Combine(Environment.CurrentDirectory, "history.json");
-
-            if (File.Exists(path))
+            peerConnection = new PeerConnection();
+            var config = new PeerConnectionConfiguration
             {
-                using (StreamReader file = File.OpenText(path))
-                {
-                    JsonSerializer serializer = new JsonSerializer();
-                    history = (Dictionary<string, Phone>)serializer.Deserialize(file, typeof(Dictionary<string, Phone>));
+                IceServers = new List<IceServer> {
+                new IceServer{ Urls = { "stun:stun.l.google.com:19302" } }
                 }
-            }
-        }
+            };
 
+            await peerConnection.InitializeAsync(config);
 
-        private void WebSocket_OnOpen(object? sender, EventArgs e) { }
+            SetUpTransceiver();
 
-        private void WebSocket_OnMessage(object? sender, WebSocketSharp.MessageEventArgs e)
-        {
-            //var converter = new ExpandoObjectConverter();
-            //dynamic data = JsonConvert.DeserializeObject<ExpandoObject>(e.Data, converter);
-            //bool isAccept = true;
-
-            //switch (data.type)
-            //{
-            //    case "server_connect":
-            //        break;
-            //    case "server_android_connect":
-            //        ConfirmConnection(data, isAccept);
-            //        break;
-            //    case "server_offer":
-            //        peerConnection.SetRemoteDescriptionAsync(data.offer);
-            //        peerConnection.CreateAnswer();
-            //        break;
-            //    case "server_candidate":
-            //        peerConnection.AddIceCandidate(data.candidate);
-            //        break;
-            //    case "server_android_disconnect":
-            //        RemoveActivePhone(data);
-            //        break;
-            //    case "server_android_close":
-            //        RemoveActivePhone(data);
-            //        break;
-            //    default:
-            //        break;
-            //}
-        }
-
-        private void ConfirmConnection(dynamic data, bool accept)
-        {
-            if (history.ContainsKey(data.mac))
+            peerConnection.VideoTrackAdded += (RemoteVideoTrack track) =>
             {
-                MessageBox.Show("Ok");
-                UpdateActivePhones(data);
-                AcceptPhone(false);
-            }
-            else
+                remoteVideoTrack = track;
+                //remoteVideoTrack.Argb32VideoFrameReady += RemoteVideoTrack_Argb32VideoFrameReady;
+            };
+
+            peerConnection.AudioTrackAdded += (RemoteAudioTrack track) =>
             {
-                if (accept)
-                {
-                    MessageBox.Show("Accept");
-                    UpdateActivePhones(data);
-                    UpdateHistory(data);
-                    AcceptPhone(true);
-                }
-                else
-                {
-                    MessageBox.Show("Reject");
-                }
-            }
+                remoteAudioTrack = track;
+                //remoteAudioTrack.AudioFrameReady += RemoteAudioTrack_AudioFrameReady;
+            };
+
+            //peerConnection.LocalSdpReadytoSend += PeerConnection_LocalSdpReadytoSend;
+            //peerConnection.IceCandidateReadytoSend += PeerConnection_IceCandidateReadytoSend;
         }
 
-        private void RemoveActivePhone(dynamic data)
+        private void SetUpTransceiver()
         {
-            if (!activePhones.ContainsKey(data.mac))
+            videoTransceiver = peerConnection.AddTransceiver(MediaKind.Video);
+            audioTransceiver = peerConnection.AddTransceiver(MediaKind.Audio);
+
+            videoTransceiver.DesiredDirection = Transceiver.Direction.SendReceive;
+            audioTransceiver.DesiredDirection = Transceiver.Direction.SendReceive;
+
+            videoTransceiver.LocalVideoTrack = localVideoTrack;
+            audioTransceiver.LocalAudioTrack = localAudioTrack;
+        }
+
+        private async void WebSocket_OnMessage(object? sender, WebSocketSharp.MessageEventArgs e)
+        {
+            var converter = new ExpandoObjectConverter();
+            dynamic data = JsonConvert.DeserializeObject<ExpandoObject>(e.Data, converter);
+
+            switch (data.type)
             {
-                activePhones.Remove(data.mac);
+                case "server_android_connect":
+                    Console.WriteLine("Android send connection request");
+                    AcceptPhone(data.mac);
+                    break;
+                case "server_offer":
+
+                    var sdpMessage = new SdpMessage()
+                    {
+                        Type = SdpMessageType.Offer,
+                        Content = data.offer.sdp
+                    };
+                    await peerConnection.SetRemoteDescriptionAsync(sdpMessage);
+                    peerConnection.CreateAnswer();
+
+                    break;
+                case "server_candidate":
+                    var iceCandidate = new IceCandidate()
+                    {
+                        SdpMid = data.sdpMid,
+                        SdpMlineIndex = (int)data.sdpMLineIndex,
+                        Content = data.candidate
+                    };
+
+                    peerConnection.AddIceCandidate(iceCandidate);
+                    Console.WriteLine("PC add ice candidate");
+
+                    break;
+                case "server_android_disconnect":
+                    DeleteWebRTCConnection();
+                    break;
+                case "server_android_close":
+                    DeleteWebRTCConnection();
+                    break;
+                default:
+                    break;
             }
         }
 
-        private void UpdateActivePhones(dynamic data)
-        {
-           
-        }
-
-        private void UpdateHistory(dynamic data)
-        {
-           
-        }
-
-        private void AcceptPhone(bool islinkedDate)
+        private void AcceptPhone(string mac)
         {
             var pcName = Environment.MachineName;
             var os = Environment.OSVersion.ToString();
-
-            string linkedDate = "";
-
-            if (islinkedDate)
-            {
-                linkedDate = DateTime.Today.ToString();
-            }
-
-            string message = PCMessage.Accept(currentMac, linkedDate);
-            bool result = connection.SendMessage(message);
-
-            if (!result)
-            {
-                MessageBox.Show("Server died");
-            }
+            string linkedDate = DateTime.Today.ToString();
+            string message = PCMessage.Accept(mac, linkedDate);
+            currentMac = mac;
+            connection.SendMessage(message);
         }
 
         private void DeleteWebRTCConnection()
         {
-            
+            peerConnection.Close();
+            peerConnection.Dispose();
+            peerConnection = null;
         }
 
-        private void WebSocket_OnError(object? sender, WebSocketSharp.ErrorEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            throw new NotImplementedException();
-        }
+            if (peerConnection != null)
+            {
+                DeleteWebRTCConnection();
+            }
 
-        private void WebSocket_OnClose(object? sender, WebSocketSharp.CloseEventArgs e)
-        {
-            MessageBox.Show("Server die");
+            if (process != null)
+            {
+                process.Kill();
+
+                foreach (var node in Process.GetProcessesByName("node"))
+                {
+                    node.Kill();
+                }
+            }
+
+            if (localVideoTrack != null)
+            {
+                localVideoTrack.Dispose();
+                webcamSource.Dispose();
+            }
+
+            if (localAudioTrack != null)
+            {
+                localAudioTrack.Dispose();
+                microphoneSource.Dispose();
+            }            
         }
     }
 }
